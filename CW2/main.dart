a1cf314b97519665e6da28e6a93d27ca2726e3d4
howsway3 <ibhuiyan3@student.gsu.edu>
2025-10-02 19:45:13 -0400

    Update main.dart

--- CW2/main.dart
+++ CW2/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
+void main() {
+  runApp(const RecipeBookApp());
+}
+
+class RecipeBookApp extends StatelessWidget {
+  const RecipeBookApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe Book',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        scaffoldBackgroundColor: Colors.grey[50],
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          centerTitle: true,
+        ),
+      ),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
